547c7457b03af5f1df83bd9b1b6bd5dbc064413b
Andrey-Fedosov <andrefedosov@gmail.com>
2023-09-05 20:54:14 +0300

    autotask: week4 - 1st attemption

--- src/HW/4/Input.tsx
+++ src/HW/4/Input.tsx
@@ -1,14 +1,22 @@
-type InputPropsType = {
-	currentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-	setCurrentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+import { ChangeEvent, useState } from "react";
+
+export type InputPropsType = {
+  currentText: string; // НУЖНО ПРОТИПИЗИРОВАТЬ
+  setCurrentText: (props: string) => void; // НУЖНО ПРОТИПИЗИРОВАТЬ
 };
 
 export const Input = (props: InputPropsType) => {
-	const onChangeHandler = (event: 'НУЖНО ПРОТИПИЗИРОВАТЬ') => {
-		// НУЖНО ДОПИСАТЬ
-	};
+  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    props.setCurrentText(event.currentTarget.value);
+    // НУЖНО ДОПИСАТЬ
+  };
 
-	// return (
-	//   <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
-	// );
+  return (
+    <input
+      id={"hw04-input"}
+      type="text"
+      value={props.currentText}
+      onChange={onChangeHandler}
+    />
+  );
 };
